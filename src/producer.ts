9ba90f5d7493d6c26f5919cdf84671c8b04abb3b
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-12-28 05:09:02 +0530

    set cli input for producers

--- src/producer.ts
+++ src/producer.ts
@@ -1,19 +1,40 @@
 import {kafka} from "./client";
+import readLine from "readline";
 
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-(async () => {
+async function init() {
     const producer = kafka.producer();
     console.log('connecting to producer...');
     await producer.connect();
     console.log('connected to producer');
 
-    await producer.send({
-        topic: "user-type",
-        messages: [
-            {
-                key: "user",
-                value: JSON.stringify({user: "arun", type: "admin"})
-            }
-        ]
+
+    rl.setPrompt("> ");
+    rl.prompt();
+
+    rl.on('line', async (line) => {
+
+        const [user, type] = line.split(',');
+
+        await producer.send({
+            topic: 'user-type',
+            messages: [
+                {
+                    partition: type === "admin" ? 0 : 1,
+                    key: 'user-details',
+                    value: JSON.stringify({user: user, type: type})
+                }
+            ],
+        });
+    }).on('close', async () => {
+        await producer.disconnect();
     });
-})();
+}
+
+init().catch((err) => {
+    console.error(err);
+});
